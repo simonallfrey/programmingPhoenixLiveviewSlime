da77d4965e570f385992e5187cf24738855437ab
Simon Allfrey <simon.allfrey@gmail.com>
2022-12-15 02:41:54 +0100

    add survery_live.ex

---
+++ lib/pentoslime_web/live/survey_live.ex
@@ -0,0 +1,18 @@
+defmodule PentoslimeWeb.SurveyLive do
+  use PentoslimeWeb, :live_view
+  alias Pentoslime.{Survey}
+  alias PentoslimeWeb.DemographicLive
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok,
+     socket
+     |> assign_demographic}
+  end
+
+  defp assign_demographic(%{assigns: %{current_user: current_user}} = socket) do
+    assign(socket,
+      :demographic,
+      Survey.get_demographic_by_user(current_user))
+  end
+end
